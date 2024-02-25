d9b610c5c264acf2190c9949adc5fba59fd75708
BryanHogan <nayrb@gmx.de>
2024-02-25 21:15:20 +0100

    adding navigation bar

---
+++ src/data/navItems.ts
@@ -0,0 +1,19 @@
+const navItems = [
+    {
+        text: "Destinations",
+        href: "#",
+        style: "ghost"
+    },
+    {
+        text: "Destinations",
+        href: "#",
+        style: "ghost"
+    },
+    {
+        text: "Destinations",
+        href: "#",
+        style: "ghost"
+    }
+];
+
+export default navItems;
