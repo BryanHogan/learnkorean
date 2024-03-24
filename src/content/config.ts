778ee1016cd382eee586edce3798e755cefca020
BryanHogan <nayrb@gmx.de>
2024-03-24 17:15:51 +0100

    made content collection

---
+++ src/content/config.ts
@@ -0,0 +1,38 @@
+import { z, defineCollection } from 'astro:content';
+
+const unitCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
+        author: z.string(),
+        description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
+        coverImage: z.object({
+            url: z.string(),
+            alt: z.string(),
+        }),
+        pubDate: z.date(),
+        lastUpdate: z.date(),
+        sortOrder: z.number(),
+        tags: z.array(z.string()),
+    })
+});
+const postCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+        title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
+        author: z.string(),
+        description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
+        coverImage: z.object({
+            url: z.string(),
+            alt: z.string(),
+        }),
+        pubDate: z.date(),
+        lastUpdate: z.date(),
+        tags: z.array(z.string()),
+    })
+});
+
+export const collections = {
+    'unit-1': unitCollection,
+    'posts': unitCollection,
+  };
---
+++ src/content/unit-1/korean-alphabet-hangul.md
@@ -0,0 +1,12 @@
+---
+title: The Korean Alphabet - Hangul
+author: Bryan Hogan
+description: I short description about trying to learn, make this a bbit longer
+coverImage:
+    url: ../assets/Unit-1.png
+    alt: Cover image
+pubDate: 2024-03-24
+lastUpdate: 2024-03-24
+tags: ["hangul", "two"]
+---
+Your blog post content here.
