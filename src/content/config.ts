1d8e9d32f2c4bcb1cc7b57e3c0803bdc9d2dc135
BryanHogan <nayrb@gmx.de>
2024-03-24 19:01:50 +0100

    making postLayout

--- src/content/config.ts
+++ src/content/config.ts
@@ -6,7 +6,7 @@ const unitCollection = defineCollection({
         title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
         author: z.string(),
         description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
-        coverImage: z.object({
+        cover: z.object({
             url: z.string(),
             alt: z.string(),
         }),
@@ -22,7 +22,7 @@ const postCollection = defineCollection({
         title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
         author: z.string(),
         description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
-        coverImage: z.object({
+        cover: z.object({
             url: z.string(),
             alt: z.string(),
         }),
--- src/content/unit-1/korean-alphabet-hangul.md
+++ src/content/unit-1/korean-alphabet-hangul.md
@@ -2,14 +2,20 @@
 title: The Korean Alphabet - Hangul
 author: Bryan Hogan
 description: I short description about trying to learn, make this a bbit longer
-coverImage:
-    url: ../assets/Unit-1.png
+cover:
+    url: /src/assets/images/Unit-1.png
     alt: Cover image
 pubDate: 2024-03-24
 lastUpdate: 2024-03-24
 tags: ["hangul", "two"]
 sortOrder: 1
 ---
+Blog post content here.
+Choosing a career is a complex process. What kind of work direction you go into has a major impact on your life. But most people never take a step back and think about it.
+
+So how to choose something? There are 3 main building blocks that can help you find direction, the 3 P’s are: Purpose, Passion and Personality.
+
+## 2 Heading two
 Blog post content here.
 Choosing a career is a complex process. What kind of work direction you go into has a major impact on your life. But most people never take a step back and think about it.
 
