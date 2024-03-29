012bb3331c1201b02396215451b580e0a3184c7f
BryanHogan <nayrb@gmx.de>
2024-03-29 15:05:49 +0100

    moved image and improved schema

--- src/content/chapters/korean-alphabet-hangul.md
+++ src/content/chapters/korean-alphabet-hangul.md
@@ -2,7 +2,7 @@
 title: The Korean Alphabet - Hangul
 author: Bryan Hogan
 description: I short description about trying to learn, make this a bbit longer
-cover: ./Unit-1.png
+cover: ../../assets/images/Unit-1.png
 coverAlt: image
 pubDate: 2024-03-24
 lastUpdate: 2024-03-24
--- src/content/config.ts
+++ src/content/config.ts
@@ -6,9 +6,13 @@ const chapterCollection = defineCollection({
         title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
         author: z.string(),
         description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
-        cover: image().refine((img) => img.width = 1600, {
-            message: "Cover image must be 1600 pixel wide!",
-        }),
+        cover: image()
+            .refine((img) => img.width = 1600, {
+                message: "Cover image must be 1600 pixel wide!",
+            })
+            .refine((img) => img.height = 900, {
+                message: "Cover image must be 900 pixel high!",
+            }),
         coverAlt: z.string(),
         pubDate: z.date(),
         lastUpdate: z.date(),
