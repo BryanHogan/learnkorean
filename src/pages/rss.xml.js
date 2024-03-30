4b2238511d7a33c737a231d2bd88fb261f40a47b
BryanHogan <nayrb@gmx.de>
2024-03-31 00:14:46 +0100

    added rss

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,19 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+    const chapters = await getCollection('chapters');
+    return rss({
+        title: "ToLearnKorean",
+        description: "Your helper to learn Korean.",
+        site: context.site,
+        items: chapters.map((post) => ({
+            title: post.data.title,
+            pubDate: post.data.pubDate,
+            description: post.data.description,
+            // Compute RSS link from post `slug`
+            // This example assumes all posts are rendered as `/blog/[slug]` routes
+            link: `/unit-1/${post.slug}/`,
+        })),
+    });
+}
