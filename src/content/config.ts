1cd3abf37f4fc4407fd251418bc7117c65f3a36b
BryanHogan <nayrb@gmx.de>
2024-05-05 17:30:57 +0200

    adding posts content to seperate from chapters

--- src/content/config.ts
+++ src/content/config.ts
@@ -28,14 +28,16 @@ const postCollection = defineCollection({
         title: z.string().max(60, "Maximum of 60 characters in title required.").min(10, "Minimum of 10 characters in title required."),
         author: z.string(),
         description: z.string().max(162, "Maximum of 162 characters in description required.").min(50, "Minimum of 50 characters in description required."),
-        coverUrl: image().refine((img) => img.width >= 10, {
-            message: "Cover image must be at least 10 pixels wide!",
-        }),
+        cover: image()
+            .refine((img) => img.width = 1600, {
+                message: "Cover image must be 1600 pixel wide!",
+            })
+            .refine((img) => img.height = 900, {
+                message: "Cover image must be 900 pixel high!",
+            }),
         coverAlt: z.string(),
         pubDate: z.date(),
         lastUpdate: z.date(),
-        unit: z.number(),
-        sortOrder: z.number(),
         tags: z.array(z.string()),
     })
 });
---
+++ src/content/posts/test-post.md
@@ -0,0 +1,14 @@
+---
+title: First post
+author: Bryan Hogan
+description: I short description about trying to learn, make this a bit longer
+cover: ../../assets/images/ToLearnKorean.com-Unit-1-Chapter-1-Korean-Alphabet-Cover.png
+coverAlt: image
+pubDate: 2024-03-24
+lastUpdate: 2024-03-24
+tags: ["test", "two"]
+---
+
+Welcome to ToLearnKorean.com, glad to see you here.
+
+Learning Korean can be hard and intimidating at times.
