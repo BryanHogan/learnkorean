2a1973e1a119854e4c957d16f2d3c5ed0a8fbfd2
BryanHogan <nayrb@gmx.de>
2024-06-08 15:20:16 +0200

    removed comments

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,26 +3,23 @@ import { getCollection } from 'astro:content';
 
 export async function GET(context) {
     const chapters = await getCollection('chapters');
-    const posts = await getCollection('posts'); // Fetch posts collection
+    const posts = await getCollection('posts');
 
-    // Combine both collections into a single array
     const items = [
         ...chapters.map((post) => ({
             title: post.data.title,
             pubDate: post.data.pubDate,
             description: post.data.description,
-            // rendered as `/unit-1/[slug]` routes, this will have to get changed in the future!
             link: `/unit-${post.data.unit}/${post.slug}`,
         })),
         ...posts.map((post) => ({
             title: post.data.title,
             pubDate: post.data.pubDate,
             description: post.data.description,
-            link: `/blog/${post.slug}`, // Adjust the link structure as needed
+            link: `/blog/${post.slug}`,
         })),
     ];
 
-    // Sort the combined items by publication date in descending order
     items.sort((a, b) => new Date(b.pubDate) - new Date(a.pubDate));
 
     return rss({
@@ -30,6 +27,6 @@ export async function GET(context) {
         description: "Your helper to learn Korean.",
         site: context.site,
         stylesheet: '/styles.xsl',
-        items, // Use the combined items array
+        items,
     });
 }
