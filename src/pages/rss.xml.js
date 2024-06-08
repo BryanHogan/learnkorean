491f992a83df190fb07d86f69578fdd9c55f1017
BryanHogan <nayrb@gmx.de>
2024-06-08 14:58:58 +0200

    fixed wrong slug for blog posts on rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -18,7 +18,7 @@ export async function GET(context) {
             title: post.data.title,
             pubDate: post.data.pubDate,
             description: post.data.description,
-            link: `/posts/${post.slug}`, // Adjust the link structure as needed
+            link: `/blog/${post.slug}`, // Adjust the link structure as needed
         })),
     ];
 
