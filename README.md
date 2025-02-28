aa8fbeb176fb9a80062dcfee60636cd6334ef179
BryanHogan <bryanhoganme@gmail.com>
2025-02-28 15:25:46 +0100

    new stack starlight

---
+++ README.md
@@ -0,0 +1,17 @@
+# A Webpage For Korean Learners - ToLearnKorean.com
+
+[![Live Site](https://img.shields.io/badge/Live%20Site-tolearnkorean.com-blue)](https://tolearnkorean.com/)
+
+Welcome to the official repository of [tolearnkorean.com](https://tolearnkorean.com/)!  
+A website dedicated to helping learners of all levels to learn Korean.
+
+## Technologies Used
+
+This project is built using the following technologies:
+
+- **[AstroJS](https://astro.build/):** A modern static site builder.
+- **[Starlight](https://starlight.astro.build/):** Tool for building documentation sites.
+
+## Contact
+
+https://bryanhogan.com/socials
---
+++ astro.config.mjs
@@ -0,0 +1,64 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://tolearnkorean.com',
+	integrations: [
+		starlight({
+			title: 'To Learn Korean',
+			logo: {
+				light: './src/assets/ToLearnKorean-Logo-Dark.svg',
+				dark: './src/assets/ToLearnKorean-Logo.svg',
+			},
+			customCss: [
+				'./src/styles/custom.css',
+			],
+			lastUpdated: true,
+			editLink: {
+				baseUrl: 'https://github.com/bryanhogan/loreankorean/edit/master/',
+			},
+			social: {
+				github: 'https://github.com/BryanHogan/learnkorean',
+				email: 'https://bryanhogan.com/socials',
+				rss: 'https://tolearnkorean.com/rss.xml'
+			},
+			sidebar: [
+				{ label: 'Introduction', link: 'chapter/introduction' },
+				{
+					label: 'Unit 0',
+					items: [
+						{ label: 'Korean Alphabet: Hangul', slug: 'chapter/korean-alphabet-hangul' },
+						{ label: 'Strong Consonants', slug: 'chapter/strong-consonants-hangul' },
+						{ label: 'Double Consonants', slug: 'chapter/double-consonants-hangul' },
+						{ label: 'Diphthongs', slug: 'chapter/diphthongs' },
+						{ label: 'Basic Words', slug: 'chapter/basic-words' },
+						{ label: 'Further Reading & Practice', slug: 'chapter/unit-0-completed' },
+					],
+				},
+				{
+					label: 'Unit 1',
+					items: [
+						{ label: 'Unit 1 Introduction', slug: 'chapter/unit-1-introduction' },
+						{ label: 'Sentence Structure', slug: 'chapter/sentence-structure' },
+					],
+				},
+				{
+					label: 'Blog',
+					items: [
+						{ label: 'Overview', slug: 'blog/overview' },
+						{ label: 'How to Anki', slug: 'blog/how-to-anki' },
+						{ label: 'Launch', slug: 'blog/launch' },
+					],
+				},
+				{
+					label: 'Miscellaneous',
+					items: [
+						{ label: 'About', slug: 'about' },
+					],
+				},
+			],
+		}),
+	],
+});
