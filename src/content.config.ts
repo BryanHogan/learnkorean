be56ea13a8110f6954337d092f4e358292c08fc2
BryanHogan <bryanhoganme@gmail.com>
2026-08-30 00:13:53 +0200

    Adjusting how routes are built and new image ref path adjusted

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,5 +3,18 @@ import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 
 export const collections = {
-	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
+	docs: defineCollection({
+		loader: docsLoader({
+			generateId: ({ entry, data }) => {
+				if (typeof data.slug === 'string') return data.slug;
+
+				return entry
+					.replace(/\\/g, '/')
+					.replace(/\.[^.]+$/, '')
+					.replace(/^chapter\/\d+\//, 'chapter/')
+					.replace(/\/index$/, '');
+			},
+		}),
+		schema: docsSchema(),
+	}),
 };
